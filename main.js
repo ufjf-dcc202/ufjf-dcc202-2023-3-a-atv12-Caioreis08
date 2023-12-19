631189901d86071d8e68137d2c99fc142e065b2d
Caio Reis <fernandes.caio@estudante.ufjf.br>
2023-12-19 13:20:00 -0300

    função ler formulario criada

--- main.js
+++ main.js
@@ -5,4 +5,18 @@ document.entrada.addEventListener('submit', leFormulario)
 const olJoao = document.querySelector("#joao")
 const olMaria = document.querySelector("#maria")
 
-atualizarTela()
+atualizaTela()
+
+function leFormulario(event){
+    event.preventDefault()
+
+    const fruta = document.entrada.fruta.value
+    const quantidade = document.entrada.quantidade.valueAsNumber
+    const origem = document.entrada.origem.value
+    const destino = document.entrada.destino.value
+
+    console.log(`solicitado: ${origem} doa ${quantidade} ${fruta} para ${destino}`)
+
+    transacaoNoEstoque(origem, destino, fruta, quantidade)
+    atualizaTela()
+}
