52dca6ee30a92a5dc421cfa5724154fcdf8c7b50
Caio Reis <fernandes.caio@estudante.ufjf.br>
2023-12-19 13:09:03 -0300

    estrutura básica adicionada

---
+++ main.js
@@ -0,0 +1,8 @@
+import { getEstoque, limpaEstoque, transacaoNoEstoque } from "./estoque"
+
+document.entrada.addEventListener('submit', leFormulario)
+
+const olJoao = document.querySelector("#joao")
+const olMaria = document.querySelector("#maria")
+
+atualizarTela()
