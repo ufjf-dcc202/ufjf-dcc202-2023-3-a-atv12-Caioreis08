197845d2392343ef17d702fe5c03a90c89795e29
Caio Reis <fernandes.caio@estudante.ufjf.br>
2023-12-19 13:36:42 -0300

    função preencheListaPessoa criada

--- main.js
+++ main.js
@@ -38,3 +38,15 @@ function atualizaTela(){
     preencheListaPessoa(estoque['maria'], olMaria)
 }
 
+function preencheListaPessoa(pessoa, lista){
+    lista.innerHTML = ""
+
+    if(Array.isArray(pessoa)) {
+        for(let i = 0; i < pessoa.length; i++){
+            const monte = pessoa[i]
+            const eLi = document.createElement('li')
+            eLi.innerText = `${monte.tipo}: ${monte.quantidade}`
+            lista.append(eLi) 
+        }
+    }
+}
